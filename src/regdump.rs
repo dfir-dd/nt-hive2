d508216051dad3fb28fae53a213928642f8a5252
Jan Starke <jan.starke@t-systems.com>
2022-03-05 17:06:39 +0100

    first implementation of vk

--- src/regdump.rs
+++ src/regdump.rs
@@ -17,11 +17,20 @@ fn print_key<RS>(keynode: &KeyNode<&Hive<RS>, RS>, path: &mut Vec<String>) -> Re
     path.push(keynode.name().unwrap().to_string());
     println!("[{}]", path.join("\\"));
 
+    print_values(keynode)?;
+
     for sk in keynode.subkeys().unwrap() {
         print_key(&sk, path)?;
     }
     path.pop();
 
+    Ok(())
+}
+
+fn print_values<RS>(keynode: &KeyNode<&Hive<RS>, RS>) -> Result<()> where RS: Read + Seek {
+    for value in keynode.values()? {
+        println!("{}", value.header.)
+    }
     Ok(())
 }
 
