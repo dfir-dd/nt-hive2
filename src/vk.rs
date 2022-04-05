3ef9566423c9dede2ed17fcea4c47e47e5373f1e
Jan Starke <jan.starke@t-systems.com>
2022-04-05 13:01:27 +0200

    replaced from_bits by from_bits_truncate

--- src/vk.rs
+++ src/vk.rs
@@ -3,7 +3,6 @@ use crate::util::SizedVec;
 use crate::Cell;
 use crate::Hive;
 use crate::Offset;
-use crate::Result;
 
 use binread::BinResult;
 use binread::PosValue;
@@ -54,7 +53,13 @@ pub struct KeyValue {
     name_length: u16,
     data_size: u32,
     data_offset: PosValue<Offset>,
-    data_type: KeyValueDataType,
+
+    /// There are also Types that do not have a value that corresponds to
+    /// anything in the list above. These are typically seen in the SAM
+    /// Registry hives and often correspond to part of a users SID
+    /// https://binaryforay.blogspot.com/2015/01/registry-hive-basics-part-3-vk-records.html
+    #[br(try)]
+    data_type: Option<KeyValueDataType>,
 
     #[br(parse_with=parse_value_flags)]
     flags: KeyValueFlags,
@@ -65,25 +70,53 @@ pub struct KeyValue {
 }
 
 /// Possible data types of the data belonging to a [`KeyValue`].
+/// https://docs.microsoft.com/en-us/windows/win32/sysinfo/registry-value-types
 #[derive(BinRead)]
 #[br(repr=u32)]
 pub enum KeyValueDataType {
+    /// Data with no particular type 
     RegNone = 0x0000_0000,
+
+    /// A null-terminated string. This will be either a Unicode or an ANSI string, depending on whether you use the Unicode or ANSI functions.
     RegSZ = 0x0000_0001,
+
+    /// A null-terminated Unicode string, containing unexpanded references to environment variables, such as "%PATH%" 
     RegExpandSZ = 0x0000_0002,
+
+    /// Binary data in any form 
     RegBinary = 0x0000_0003,
+
+    /// A 4-byte numerical value 
     RegDWord = 0x0000_0004,
+
+    /// A 4-byte numerical value whose least significant byte is at the highest address 
     RegDWordBigEndian = 0x0000_0005,
+
+    /// A Unicode string naming a symbolic link. This type is irrelevant to device and intermediate drivers 
     RegLink = 0x0000_0006,
+
+    /// An array of null-terminated strings, terminated by another zero 
     RegMultiSZ = 0x0000_0007,
+
+    /// A device driver's list of hardware resources, used by the driver or one of the physical devices it controls, in the \ResourceMap tree 
     RegResourceList = 0x0000_0008,
+
+    /// A list of hardware resources that a physical device is using, detected and written into the \HardwareDescription tree by the system 
     RegFullResourceDescriptor = 0x0000_0009,
+
+    /// A device driver's list of possible hardware resources it or one of the physical devices it controls can use, from which the system writes a subset into the \ResourceMap tree 
     RegResourceRequirementsList = 0x0000_000a,
+
+    /// A 64-bit number.
     RegQWord = 0x0000_000b,
+
+    /// FILETIME data
+    RegFileTime = 0x0000_0010,
 }
 
 pub enum RegistryValue {
     RegNone,
+    RegUnknown,
     RegSZ(String),
     RegExpandSZ(String),
     RegBinary(Vec<u8>),
@@ -95,11 +128,13 @@ pub enum RegistryValue {
     RegFullResourceDescriptor(String),
     RegResourceRequirementsList(String),
     RegQWord(u64),
+    RegFileTime,
 }
 
 impl Display for RegistryValue {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            RegistryValue::RegUnknown => write!(f, "Unknown"),
             RegistryValue::RegNone => write!(f, "None"),
             RegistryValue::RegSZ(val) => write!(f, "\"{}\"", val),
             RegistryValue::RegExpandSZ(val) => write!(f, "\"{}\"", val),
@@ -112,6 +147,7 @@ impl Display for RegistryValue {
             RegistryValue::RegFullResourceDescriptor(val) => write!(f, "\"{}\"", val),
             RegistryValue::RegResourceRequirementsList(val) => write!(f, "\"{}\"", val),
             RegistryValue::RegQWord(val) => write!(f, "qword:0x{:016x}", val),
+            RegistryValue::RegFileTime => todo!(),
         }
     }
 }
@@ -121,11 +157,16 @@ impl KeyValue {
         &self.key_name_string
     }
 
-    pub fn value<B>(&self, hive: &mut Hive<B>) -> Result<RegistryValue>
+    pub fn value<B>(&self, hive: &mut Hive<B>) -> BinResult<RegistryValue>
     where
         B: BinReaderExt,
     {
-        if matches!(self.data_type, KeyValueDataType::RegNone) {
+        let data_type = match &self.data_type {
+            None => return Ok(RegistryValue::RegUnknown),
+            Some(dt) => dt
+        };
+
+        if matches!(data_type, KeyValueDataType::RegNone) {
             return Ok(RegistryValue::RegNone);
         }
 
@@ -148,7 +189,7 @@ impl KeyValue {
         };
 
         let result = 
-        match self.data_type {
+        match data_type {
             KeyValueDataType::RegNone => RegistryValue::RegNone,
             KeyValueDataType::RegSZ => RegistryValue::RegSZ(parse_reg_sz(&raw_value[..])?),
             KeyValueDataType::RegExpandSZ => RegistryValue::RegExpandSZ(parse_reg_sz(&raw_value[..])?),
@@ -161,6 +202,7 @@ impl KeyValue {
             KeyValueDataType::RegFullResourceDescriptor => RegistryValue::RegNone,
             KeyValueDataType::RegResourceRequirementsList => RegistryValue::RegNone,
             KeyValueDataType::RegQWord => RegistryValue::RegQWord(Cursor::new(raw_value).read_le()?),
+            KeyValueDataType::RegFileTime => RegistryValue::RegFileTime,
         };
 
         Ok(result)
@@ -173,7 +215,7 @@ fn parse_value_flags<R: Read + Seek>(
     _: (),
 ) -> BinResult<KeyValueFlags> {
     let raw_value: u16 = reader.read_le()?;
-    Ok(KeyValueFlags::from_bits(raw_value).unwrap())
+    Ok(KeyValueFlags::from_bits_truncate(raw_value))
 }
 
 impl From<Cell<KeyValue>> for KeyValue {
