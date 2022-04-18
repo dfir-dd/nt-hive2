b7b7263fbcc97e3ef3302a46d324c78606943584
Jan Starke <jan.starke@t-systems.com>
2022-04-18 20:30:54 +0200

    removed from_offset_args

--- src/cell.rs
+++ src/cell.rs
@@ -1,11 +1,6 @@
 
-use binread::{BinReaderExt, BinRead, BinResult};
-
-use crate::Hive;
-use crate::Offset;
+use binread::BinRead;
 use std::any::Any;
-use std::io::{Seek, SeekFrom};
-use std::ops::DerefMut;
 
 
 #[derive(BinRead)]
@@ -35,6 +30,7 @@ impl CellHeader {
 pub struct Cell<T, A: Any + Copy>
 where
     T: BinRead<Args=A>, {
+    
     header: CellHeader,
 
     #[br(args_tuple(data_args))]
@@ -60,18 +56,5 @@ impl<T, A> Cell<T, A> where T: BinRead<Args=A>, A: Any + Copy {
 
     pub(crate) fn into_data(self) -> T {
         self.data
-    }
-
-    pub fn from_offset_args<H, B>(mut hive: H, offset: Offset, args: T::Args) -> BinResult<Cell<T, A>> where
-        H: DerefMut<Target = Hive<B>>,
-        B: BinReaderExt
-    {
-        let _offset = hive.seek(SeekFrom::Start(offset.0.into()))?;
-        let header: CellHeader = hive.read_le()?;
-        let data: T = hive.read_le_args(args)?;
-        Ok(Cell::<T, A> {
-            header,
-            data,
-        })
     }
 }
