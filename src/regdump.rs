15a41f75437caeb89de04a6237a237a13657f1f8
Jan Starke <jan.starke@t-systems.com>
2022-04-18 21:35:14 +0200

    added commandline options

--- src/regdump.rs
+++ src/regdump.rs
@@ -1,16 +1,40 @@
 use nt_hive2::*;
+use simplelog::{SimpleLogger, Config};
 use std::fs::File;
-use std::io::{Read, Cursor, Seek};
+use std::io::{Read, Seek};
 use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// name of the file to dump
+    pub (crate) hive_file: String,
+
+    #[clap(flatten)]
+    pub (crate) verbose: clap_verbosity_flag::Verbosity,
+}
 
 fn main() {
-    env_logger::init();
-        
-    let testhive = testhive_vec();
-    let mut hive = Hive::new(Cursor::new(testhive)).unwrap();
-    let mut path = Vec::new();
-    let root_key = &hive.root_key_node().unwrap();
-    print_key(&mut hive, &root_key, &mut path).unwrap();
+    let cli = Args::parse();
+    let _ = SimpleLogger::init(cli.verbose.log_level_filter(), Config::default());
+
+    fn do_print_key<RS>(hive: &mut Hive<RS>, root_key: &KeyNode) -> Result<()> where RS: Read + Seek {
+        let mut path = Vec::new();
+        print_key(hive, &root_key, &mut path)
+    }
+
+    match File::open(&cli.hive_file) {
+        Ok(data) => {
+            let mut hive = Hive::new(data).unwrap();
+            let root_key = &hive.root_key_node().unwrap();
+            do_print_key(&mut hive, &root_key).unwrap();
+        }
+        Err(why) => {
+            eprintln!("unable to open '{}': {}", cli.hive_file, why);
+            std::process::exit(-1);
+        },
+    }
 }
 
 fn print_key<RS>(hive: &mut Hive<RS>, keynode: &KeyNode, path: &mut Vec<String>) -> Result<()> where RS: Read + Seek {
@@ -31,14 +55,4 @@ fn print_values(keynode: &KeyNode) {
     for value in keynode.values() {
         println!("\"{}\" = {}", value.name(), value.value());
     }
-}
-
-
-pub fn testhive_vec() -> Vec<u8> {
-    let mut buffer = Vec::new();
-    File::open("tests/data/SYSTEM")
-        .unwrap()
-        .read_to_end(&mut buffer)
-        .unwrap();
-    buffer
 }
