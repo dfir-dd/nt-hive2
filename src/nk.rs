b0360e545f910686185c09d61e39b3d3bba78d0a
Jan Starke <jan.starke@t-systems.com>
2022-04-20 21:41:33 +0200

    removed one debug print

--- src/nk.rs
+++ src/nk.rs
@@ -202,7 +202,6 @@ impl KeyNode
     
 
     fn subpath_parts<B>(&self, mut path_parts: Vec<&str>, hive: &mut Hive<B>) -> BinResult<Option<Rc<RefCell<Self>>>> where B: BinReaderExt {
-        eprintln!("subpath_parts({:?}): BEGIN", path_parts);
         if let Some(first) = path_parts.pop() {
             if let Some(top) = self.subkey(first, hive)? {
                 return if path_parts.is_empty() {
