8ef0c3c348c0930def8a4c265c6b5393df5d826d
Jan Starke <jan.starke@telekom.de>
2023-07-25 14:40:32 +0200

    remove binary crates

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # nt_hive2
 
-
 > **Warning**
-> This Repository has been moved to <https://github.com/janstarke/dfir-toolkit>
+> The tools of this repository have been moved to <https://github.com/dfir-dd/dfir-toolkit>
 >
 > You can install the tools by running `cargo install dfir-toolkit`
 > 
+> The lib itself will stay available here
 
 
 This crates aims to be a replacement of <https://github.com/ColinFinck/nt-hive>, with the following
@@ -14,65 +14,6 @@ differences:
  - use of [BinRead](https://docs.rs/binread/latest/binread/) to parse hive files
  - support of displaying last written timestamps
  - recovery of deleted cells
-
-# `regdump`
-
-```
-forensic parser library for Windows registry hive files
-
-Usage: regdump [OPTIONS] <HIVE_FILE>
-
-Arguments:
-  <HIVE_FILE>  name of the file to dump
-
-Options:
-  -L, --log <LOGFILES>     transaction LOG file(s). This argument can be specified one or two times
-  -b, --bodyfile           print as bodyfile format
-  -I, --ignore-base-block  ignore the base block (e.g. if it was encrypted by some ransomware)
-  -v, --verbose...         More output per occurrence
-  -q, --quiet...           Less output per occurrence
-  -h, --help               Print help information
-  -V, --version            Print version information
-
-```
-
-# `hivescan`
-
-```
-scans a registry hive file for deleted entries
-
-Usage: hivescan [OPTIONS] <HIVE_FILE>
-
-Arguments:
-  <HIVE_FILE>  name of the file to scan
-
-Options:
-  -L, --log <LOGFILES>  transaction LOG file(s). This argument can be specified one or two times
-  -v, --verbose...      More output per occurrence
-  -q, --quiet...        Less output per occurrence
-  -b                    output as bodyfile format
-  -h, --help            Print help information
-  -V, --version         Print version information
-```
-
-# `cleanhive`
-
-```
-merges logfiles into a hive file
-
-Usage: cleanhive [OPTIONS] --output <DST_HIVE> <HIVE_FILE>
-
-Arguments:
-  <HIVE_FILE>  name of the file to dump
-
-Options:
-  -L, --log <LOGFILES>     transaction LOG file(s). This argument can be specified one or two times
-  -v, --verbose...         More output per occurrence
-  -q, --quiet...           Less output per occurrence
-  -O, --output <DST_HIVE>  name of the file to which the cleaned hive will be written
-  -h, --help               Print help information
-  -V, --version            Print version information
-```
 
 ## Usage example for developers
 
--- examples/dirty.rs
+++ examples/dirty.rs
@@ -1,11 +1,11 @@
-use std::{fs::File};
+use std::fs::File;
 use std::path::PathBuf;
 extern crate nt_hive2;
+use crate::nt_hive2::{BaseBlock, ContainsHive};
 use binwrite::BinWrite;
 use log::LevelFilter;
 use nt_hive2::*;
-use simplelog::{SimpleLogger, Config};
-use crate::nt_hive2::{ContainsHive, BaseBlock};
+use simplelog::{Config, SimpleLogger};
 
 macro_rules! path {
     ($base: expr, $($part:expr), *) => ({
@@ -20,25 +20,50 @@ macro_rules! path {
 fn main() -> anyhow::Result<()> {
     let _ = SimpleLogger::init(LevelFilter::Trace, Config::default());
 
-    let path_clean = path!(env!("CARGO_MANIFEST_DIR"), "tests", "data", "NewDirtyHive1.clean");
-    let path_hive = path!(env!("CARGO_MANIFEST_DIR"), "tests", "data", "NewDirtyHive1", "NewDirtyHive");
-    let path_log1 = path!(env!("CARGO_MANIFEST_DIR"), "tests", "data", "NewDirtyHive1", "NewDirtyHive.LOG1");
-    let path_log2 = path!(env!("CARGO_MANIFEST_DIR"), "tests", "data", "NewDirtyHive1", "NewDirtyHive.LOG2");
+    let path_clean = path!(
+        env!("CARGO_MANIFEST_DIR"),
+        "tests",
+        "data",
+        "NewDirtyHive1.clean"
+    );
+    let path_hive = path!(
+        env!("CARGO_MANIFEST_DIR"),
+        "tests",
+        "data",
+        "NewDirtyHive1",
+        "NewDirtyHive"
+    );
+    let path_log1 = path!(
+        env!("CARGO_MANIFEST_DIR"),
+        "tests",
+        "data",
+        "NewDirtyHive1",
+        "NewDirtyHive.LOG1"
+    );
+    let path_log2 = path!(
+        env!("CARGO_MANIFEST_DIR"),
+        "tests",
+        "data",
+        "NewDirtyHive1",
+        "NewDirtyHive.LOG2"
+    );
 
     let hive_file = File::open(path_hive)?;
     let hive_log1 = File::open(path_log1)?;
     let hive_log2 = File::open(path_log2)?;
-    let mut hive = 
-        Hive::new(&hive_file, nt_hive2::HiveParseMode::NormalWithBaseBlock)?
+    let mut hive = Hive::new(&hive_file, nt_hive2::HiveParseMode::NormalWithBaseBlock)?
         .with_transaction_log(hive_log1.try_into()?)?
         .with_transaction_log(hive_log2.try_into()?)?
         .apply_logs();
-        //.treat_hive_as_clean();
+    //.treat_hive_as_clean();
 
     let mut dst = File::create(path_clean)?;
     let base_block = hive.base_block().unwrap();
     log::info!("write with checksum 0x{:08x}", base_block.checksum());
-    log::info!("write with offset 0x{:08x}", base_block.root_cell_offset().0);
+    log::info!(
+        "write with offset 0x{:08x}",
+        base_block.root_cell_offset().0
+    );
     log::info!("write with data size 0x{:08x}", base_block.data_size());
     base_block.write(&mut dst)?;
     std::io::copy(&mut hive, &mut dst)?;
